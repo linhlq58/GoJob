dbbc4724f78af97b7af59edd62c41a3127561162
meo3the <namtuocnam@gmail.com>
2016-08-01 23:54:52 +0700

    Import some lib, Create login form

---
+++ app/src/main/java/com/example/linhlee/gojob/activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.linhlee.gojob.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.linhlee.gojob.R;
+
+/**
+ * Created by namvp58uet on 01/08/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
