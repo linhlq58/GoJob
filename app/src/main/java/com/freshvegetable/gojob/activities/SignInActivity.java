f3fb6ceefe76a95ace91dc6dc6b817c811ccbdbf
namvp_58 <namvp_58@vnu.edu.vn>
2016-08-04 14:07:20 +0700

    fix Sign in & sign up screen

--- app/src/main/java/com/freshvegetable/gojob/activities/SignInActivity.java
+++ app/src/main/java/com/freshvegetable/gojob/activities/SignInActivity.java
@@ -1,15 +1,13 @@
 package com.freshvegetable.gojob.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.freshvegetable.gojob.R;
@@ -34,21 +32,12 @@ import cz.msebera.android.httpclient.Header;
 public class SignInActivity extends AppCompatActivity {
 
 
-    @BindView(R.id.imgUser)
-    ImageView imgUser;
     @BindView(R.id.etSignInUsername)
     EditText etSignInUsername;
-    @BindView(R.id.imgPassword)
-    ImageView imgPassword;
     @BindView(R.id.etSignInPassword)
     EditText etSignInPassword;
-    @BindView(R.id.btnSignIn)
-    Button btnSignIn;
-    @BindView(R.id.btnSignUp)
-    Button btnSignUp;
     @BindView(R.id.container)
     RelativeLayout container;
-    private Context mContext;
     private AsyncHttpClient client = null;
 
     @Override
@@ -60,9 +49,9 @@ public class SignInActivity extends AppCompatActivity {
         client = new AsyncHttpClient();
     }
 
-    @OnClick({R.id.btnSignIn, R.id.btnSignUp})
+    @OnClick({R.id.btnSignIn, R.id.btnSignUp, R.id.tvForgetPassword, R.id.btnFacebook, R.id.btnGooglePlus})
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btnSignIn:
                 if (etSignInUsername.getText().toString().equals("")) {
                     Snackbar.make(container, "Please enter your Username", Snackbar.LENGTH_SHORT).setAction("OKAY", null).show();
@@ -80,6 +69,7 @@ public class SignInActivity extends AppCompatActivity {
                             String responce = new String(responseBody, StandardCharsets.UTF_8);
                             try {
                                 JSONObject jObject = new JSONObject(responce);
+                                Log.d("result",jObject.toString());
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
--- app/src/main/java/com/freshvegetable/gojob/activities/SignUpActivity.java
+++ app/src/main/java/com/freshvegetable/gojob/activities/SignUpActivity.java
@@ -3,16 +3,32 @@ package com.freshvegetable.gojob.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.freshvegetable.gojob.R;
+
+import butterknife.OnClick;
 
 /**
  * Created by Nam on 8/4/2016.
  */
 public class SignUpActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    @OnClick({R.id.btnSignUp, R.id.tvSignIn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSignUp:
+                break;
+            case R.id.tvSignIn:
+                this.finish();
+                break;
+        }
     }
 }
