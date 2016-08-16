70f710702f732f24aa4c3c85741692953577d73e
meo3the <namvp_58@vnu.edu.vn>
2016-08-16 16:00:27 +0700

    hide fab when scrolling

--- app/src/main/java/com/freshvegetable/gojob/widgets/ScrollingFABBehavior.java
+++ app/src/main/java/com/freshvegetable/gojob/widgets/ScrollingFABBehavior.java
@@ -1,13 +1,11 @@
 package com.freshvegetable.gojob.widgets;
 
 import android.content.Context;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
-
-import com.google.repacked.antlr.v4.misc.Utils;
 
 /**
  * Created by Nam on 8/11/2016.
@@ -17,16 +15,23 @@ public class ScrollingFABBehavior extends CoordinatorLayout.Behavior<FloatingAct
     public ScrollingFABBehavior(Context context, AttributeSet attrs) {
         super();
     }
-    @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
-                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
-                dyUnconsumed);
 
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                        nestedScrollAxes);
+    }
+
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
             child.hide();
         } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             child.show();
         }
     }
+
 }
