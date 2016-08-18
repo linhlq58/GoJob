5c227f749d74b33cff85cf101ea13a51416b2daf
meo3the <namvp_58@vnu.edu.vn>
2016-08-18 17:33:04 +0700

    new post screen

---
+++ app/src/main/java/com/freshvegetable/gojob/activities/NewPostActivity.java
@@ -0,0 +1,37 @@
+package com.freshvegetable.gojob.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.freshvegetable.gojob.R;
+
+//import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by NamVp on 18/08/2016.
+ */
+public class NewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
