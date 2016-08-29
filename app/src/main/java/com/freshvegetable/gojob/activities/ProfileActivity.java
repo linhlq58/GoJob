c7aa392368f00f68267273b073cf87ecc2e0b144
Linh Le <lequyenlinh110@gmail.com>
2016-08-29 19:08:54 +0700

    Làm cái Profile Screen. Ahihi đồ ngốc =))))

---
+++ app/src/main/java/com/freshvegetable/gojob/activities/ProfileActivity.java
@@ -0,0 +1,28 @@
+package com.freshvegetable.gojob.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.freshvegetable.gojob.R;
+
+/**
+ * Created by Linh Lee on 8/29/2016.
+ */
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        setStatusBarTranslucent(true);
+    }
+
+    private void setStatusBarTranslucent(boolean makeTranslucent) {
+        if (makeTranslucent) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+}
